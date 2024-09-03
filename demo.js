4c9d211859d3d22efeb23165235eeaf11e9268a9
Jashwanthi37 <gjashwanthi9@gmail.com>
2024-09-03 14:43:31 +0530

    tasks

---
+++ demo.js
@@ -0,0 +1,22 @@
+//To print "Hello world!"
+var a='"Hello World!"';
+console.log(a);
+
+//task1
+var age=10;
+result=(age>=5&&age<=12)?"The Admission fee is 10$":"The Admission Fee is 20$";
+console.log(result);
+
+//task2
+ var time="22:00"
+ result=(time>="5:00"&&time<="11:59")?"Good Morning":(time>="12:00"&&time<="17:59Pm")?"Good Afternoon":(time>="18:00"&&time<="21:59")?"Good Evening":"Good Night";
+console.log(result);
+
+//task3
+var n=-80;
+result=(n>0)?"The number is postive":(n<0)?"The number is negative":"The number is Zero";
+console.log(result);
+
+
+
+
